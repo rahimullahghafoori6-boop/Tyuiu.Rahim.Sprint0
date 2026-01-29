8d34813d932fee910bb38921631ed70f048259fc
Ahmadi1 <Ahmadi@DS10BK3>
2026-01-29 00:58:52 -0800

    Task3

---
+++ Tyuiu.Rahim.Sprint0.Task3.V1.Test/DataServiceTest.cs
@@ -0,0 +1,17 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using System;
+
+using Tyuiu.Rahim.Sprint0.Task3.V1.Lib;
+
+namespace Tyuiu.Ahmadi1.Sprint0.Task3.V0.Tast
+{
+    [TestClass]
+    public class DataServiceTast
+    {
+        [TestMethod]
+        public void TestMethod1()
+        {
+            Assert.AreEqual(10, DataService.Sum(5, 5));
+        }
+    }
+}
