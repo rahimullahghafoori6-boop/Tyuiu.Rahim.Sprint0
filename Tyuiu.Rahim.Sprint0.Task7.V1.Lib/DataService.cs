7874b5c06dcc1763b8f4dd95c529ad63e1ed1d33
Ahmadi1 <Ahmadi@DS10BK3>
2026-01-29 06:00:43 -0800

    Task7

---
+++ Tyuiu.Rahim.Sprint0.Task7.V1.Lib/DataService.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Runtime.InteropServices.ComTypes;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Tyuiu.Rahim.Sprint0.Task7.V1.Lib
+{
+    public class DataService
+    {
+
+
+        //Пример циклической структыры (цикл с параметрами) for
+        public static int[] AdditionArrays(int[] numOne, int[] numTwo)
+        {
+            int[] resultArray = new int[5];
+            for (var i = 0; i < resultArray.Length; i++)
+            {
+                resultArray[i] = numOne[i] + numTwo[i];
+            }
+            return resultArray;
+        }
+    }
+}
